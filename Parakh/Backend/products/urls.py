9b3f70042d2c615538db3d24bb6be0c2ffe62525
kenishapacheriwala-hub <kenishapacheriwala@gmail.com>
2026-08-31 19:40:39 +0530

    base backend

--- Parakh/Backend/products/urls.py
+++ Parakh/Backend/products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('result/<int:scan_id>/', views.result_view, name='result'),
     path('history/', views.history_view, name='history'),
     path('rules/', views.rules_view, name='rules'),
+    # path('login/'),
 ]
